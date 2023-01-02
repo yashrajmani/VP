d715ee043fdf8b394dcdc98c7f0edb15c105ebec
Yash Raj Mani <yashrajmani15946@gmail.com>
2023-01-03 00:14:00 +0530

    d2

--- cpp.cpp
+++ cpp.cpp
@@ -8,12 +8,10 @@
 //     vector<int> ftr1;
 //     vector<int> ftr2;
 
-
 //     int n;
 //     cin>>n;
 
 //     int x=0;
-
 
 //     for(int i=1; i<=n; i++)
 //     {
@@ -23,16 +21,10 @@
 
 //             x++;
 
-
-
 //         }
 //     }
 
-    
-
-
 //     int hl=ftr.size()/2;
-
 
 //     for(int i=0; i<hl; i++)
 //     {
@@ -40,9 +32,7 @@
 //         // cout<<ftr.at(i)<<endl;
 //         ftr1.push_back(ftr.at(i));
 
-
 //     }
-
 
 //     for(int i=x-1; i>=hl; i--)
 //     {
@@ -50,9 +40,7 @@
 //         // cout<<ftr.at(i)<<endl;
 //         ftr2.push_back(ftr.at(i));
 
-
 //     }
-
 
 //     for(int i=0;i<hl;i++)
 //     {
@@ -60,52 +48,88 @@
 //         cout<<ftr1.at(i)<<","<<ftr2.at(i)<<endl;
 //     }
 
+// return 0;
+// }
 
-   
+// #include <iostream>
+// #include <iomanip>
+// using namespace std;
+// int main(){
+
+//     float a,b,c,n;
+//     cin>>a>>b>>c>>n;
+//     float y;
+
+//     // ax+by+c=0;
+
+//     // for(float i=1;i<=n;i=i+2){
+// float x=0,i=1;
+
+//         while(x!=n)
+//         {
+
+//      y=(a*i+c)/b;
+//      y=-(y);
+//             // cout<<setprecision(3)<<(y)<<endl;
+//             printf("%.2f\n",y);
+//             i=i+2;
+//             x++;
+
+//         }
+//             // a*i+b*(y)+c=0;
+//             // a*i+c=-b*y;
+//             // y=(a*i+c)/b;
+//             // cout<<y<<endl;
+
+//     // }
 
 // return 0;
 // }
 
- 
 #include <iostream>
-#include <iomanip>
+#include <string>
 using namespace std;
-int main(){
+int main()
+{
 
-    float a,b,c,n;
-    cin>>a>>b>>c>>n;
-    float y;
+    string s;
+    cin >> s;
+    int val = 0;
 
-    // ax+by+c=0;
+    int d = s.size();
 
+    string td = "";
 
-    // for(float i=1;i<=n;i=i+2){
-float x=0,i=1;
+    for (int i = 0; i < s.size(); i++)
+    {
 
-        while(x!=n)
+        td += s[i];
+
+        int x = stoi(td);
+
+        if (x % d == 0)
         {
-            
-     y=(a*i+c)/b;
-     y=-(y);
-            // cout<<setprecision(3)<<(y)<<endl;
-            printf("%.2f\n",y);
-            i=i+2;
-            x++;
 
+            // cout << x << "%" << d << "YES" << endl;
+            val++;
         }
-            // a*i+b*(y)+c=0;
-            // a*i+c=-b*y;
-            // y=(a*i+c)/b;
-            // cout<<y<<endl;
+        else
+        {
+            // cout << x << "%" << d << "NO" << endl;
+        }
+        d--;
 
+        //cout << td << endl;
+    }
 
+    if (val == s.size())
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
 
-
-
-
-    // }
-
-
-   
-return 0;
+    return 0;
 }
